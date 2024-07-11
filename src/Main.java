c73b310b3d18cdc0751b60708fb9d6d67e5a1a8e
daehyeokyou <ydh4085@naver.com>
2024-07-11 16:41:59 +0900

    JAVA Collection Framework Study (List, Queue, Set, Map,,)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        //System.out.println("Hello World!!");
+
+        // Collection Test
+//        CollectionStudy.listStudy();
+//        CollectionStudy.queueStudy();
+//        CollectionStudy.setStudy();
+//        try { CollectionStudy.synchronizedStudy(); }catch (InterruptedException ie) {}
+//        CollectionStudy.mapStudy();
+
     }
 }
