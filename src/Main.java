b3fb59c32b5e3cf749efd40962a02737ae9cdcf3
daehyeokyou <ydh4085@naver.com>
2024-07-16 14:10:53 +0900

    JAVA Lambda Study (Lambda, Functional Interface, Method Reference)

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello World!!");
 
-        /* Collection Test */
+        /* Collection Study */
 //        CollectionStudy.listStudy();
 //        CollectionStudy.queueStudy();
 //        CollectionStudy.setStudy();
 //        try { CollectionStudy.synchronizedStudy(); }catch (InterruptedException ie) {}
 //        CollectionStudy.mapStudy();
 
-        /* Stream Test */
+        /* Stream Study */
 //        StreamStudy.streamCreateStudy();
 //        StreamStudy.streamTypeStudy();
 //        StreamStudy.streamManufactureStudy();
 //        StreamStudy.streamDoneStudy();
 //        StreamStudy.advancedStreamStudy();
+
+        /* Lambda Study */
+        LambdaStudy.lambdaStudy();
+        //LambdaStudy.functionalInterfaceStudy();
     }
 }
