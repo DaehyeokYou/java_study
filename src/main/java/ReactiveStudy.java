2387e0fd0622e6fa20c0a4b50118fd978af854a0
daehyeokyou <ydh4085@naver.com>
2024-08-06 17:19:52 +0900

    JAVA Reactive Study (Flux, Mono)

--- src/main/java/ReactiveStudy.java
+++ src/main/java/ReactiveStudy.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
@@ -47,7 +48,57 @@ public class ReactiveStudy {
                 .forEach((i) -> System.out.println("Stream Subsc:" + i + "!! Thread Name: " + Thread.currentThread()));
 
         System.out.println("Stream End");
+        System.out.println("============================================");
 
 
+        // 변환 Study
+        // 1. map: 동기 처리 및 요소를 직접 변환
+        Mono<String> mapStudyMono = Mono.just("map study");
+        Mono<String> mapStudyMono2 = mapStudyMono.map(String::toUpperCase);
+        mapStudyMono2 = mapStudyMono.map(str -> str.toUpperCase());
+        mapStudyMono2.subscribe(System.out::println);
+
+        Flux<Integer> mapStudyFlux = Flux.range(1,3);
+        Flux<Integer> mapStudyFlux2 = mapStudyFlux.map(i -> i * i);
+        mapStudyFlux2 = mapStudyFlux.map(i -> {
+            int result;
+            result = i * i;
+            return result;
+        });
+        mapStudyFlux2.subscribe(System.out::println);
+
+        // 2. flatmap: 비동기 처리 및 요소를 이용하여 Mono로 반환
+        Mono<String> flatmapStudyMono = Mono.just("flatmap study");
+        Mono<String> flatmapStudyMono2 = mapStudyMono.flatMap(str -> Mono.just(str.toUpperCase()));
+        flatmapStudyMono2.subscribe(System.out::println);
+
+        Flux<Integer> flatmapStudyFlux = Flux.range(1,3);
+        Flux<Integer> flatmapStudyFlux2 = flatmapStudyFlux.flatMap(i -> Mono.just(i * i));
+        flatmapStudyFlux2.subscribe(System.out::println);
+
+        // 3. flatMapIterable: 요소를 이용하여 list와 같은 iterable을 만들고 iterable이 Flux로 변환
+        Mono<String> flatmapIterableMono = Mono.just("flatmap iterable study");
+        Flux<String> flatmapIterableMono2 = flatmapIterableMono.flatMapIterable(str -> List.of(str, "add: "+ str));
+        flatmapIterableMono2.subscribe(System.out::println);
+
+        Flux<Integer> flatmapIterableStudyFlux = Flux.range(1,3);
+        Flux<Integer> flatmapIterableStudyFlux2 = flatmapIterableStudyFlux.flatMapIterable(i -> List.of(i, i * i));
+        flatmapIterableStudyFlux2.subscribe(System.out::println);
+
+        // 4. flatMapMany(Mono만 사용가능): 요소를 이용하여 Flux를 만들어서 변환
+        Mono<Integer> flatmapManyStudyMono = Mono.just(1);
+        Flux<Integer> flatmapManyStudyMono2 = flatmapManyStudyMono.flatMapMany(i -> Flux.range(1,3));
+        flatmapManyStudyMono2.subscribe(System.out::println);
+
+        // 5. flatMapMany 활용 (4의 Mono 활용)
+        System.out.println("============================================");
+        Flux<Integer> flatMapManyExam = flatmapManyStudyMono.flatMapMany(i -> Flux.range(1,3).map(e -> e * e));
+        flatMapManyExam.subscribe(System.out::println); // 1, 4, 9
+
+        Flux<Integer> flatMapManyExam2 = flatmapManyStudyMono.flatMapMany(i -> Flux.range(1,3).flatMap(e -> Mono.just(e * e)));
+        flatMapManyExam2.subscribe(System.out::println); // 1, 4, 9
+
+        Flux<Integer> flatMapManyExam3 = flatmapManyStudyMono.flatMapMany(i -> Flux.range(1,3).flatMapIterable(e -> List.of(e * e)));
+        flatMapManyExam3.subscribe(System.out::println); // 1, 4, 9
     }
 }
